b49c9aa63b51ef618f8159de36b4599844afc17b
Samer Senbol <samer.senbol@gmail.com>
2020-01-02 17:39:14 +0100

    Using the model to retrieve data from the table instead of using the table name.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Product;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
@@ -15,7 +16,7 @@ class ProductsController extends Controller
                     1=>["name"=>"Galaxy","category"=>"smart phones","price"=>500],
                     2=>["name"=>"Sony","category"=>"smart phones","price"=>200]];
         */
-        $products = DB::table('products')->get();
+        $products = Product::all();
         return view("allproducts",compact("products"));
     }
 }
--- resources/views/allproducts.blade.php
+++ resources/views/allproducts.blade.php
@@ -1,4 +1,5 @@
 @foreach ($products as $product)
     <p>{{ $product->name}}</p>
     <p>{{ $product->price}}</p>
+    <p>{{ $product->description}}</p>
 @endforeach
